c674cc9144bdbd7d34f79408b39f18ab44906f85
MinhNH57 <nutmsotn04@gmail.com>
2024-08-11 13:12:03 +0700

    1

---
+++ ASP.NET Core MVC (C#4)/DemoGH_OnTap/Controllers/GHCTController.cs
@@ -0,0 +1,34 @@
+﻿using DemoGH_OnTap.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoGH_OnTap.Controllers
+{
+    public class GHCTController : Controller
+    {
+        private readonly SD18406CartDbContext _db;
+        public GHCTController(SD18406CartDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var user = HttpContext.Session.GetString("username");
+            // Đầu tiên Check xem trong Session có dữ liệu đăng nhập chưa?
+            if (user == null)
+            {
+                //var dataSession = HttpContext.Session.GetString("ghSession");
+                //var ghcts = JsonConvert.DeserializeObject<List<GHCT>>(dataSession);
+                return Content("Chưa đăng nhập không có giỏ đâu");
+            }
+            else
+            {
+                var getUser = _db.Accounts.FirstOrDefault(p => p.UserName == user);
+                var gioHang = _db.GioHang.FirstOrDefault(x => x.AccountID == getUser.Id);
+                var GHCTdata = _db.GHCTs.Where(p => p.GioHangId == gioHang.Id).ToList();
+                return View(GHCTdata);
+            }
+          
+            
+        }
+    }
+}
---
+++ ASP.NET Core MVC (C#4)/DemoGH_OnTap/Models/GHCT.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoGH_OnTap.Models
+{
+    public class GHCT
+    {
+        public Guid Id { get; set; }
+        public Guid? SanPhamId { get; set; }  // Thêm thuộc tính này
+        
+        public Guid? GioHangId { get; set; }  // Thêm thuộc tính này
+        public int Amount { get; set; }
+        public GioHang? GioHang { get; set; }  ///khi tạo câu lệnh này sẽ tự động tạo khóa ngoại => đại diện khóa ngoại luon
+        public SanPham? SanPham { get; set; }
+    }
+}
