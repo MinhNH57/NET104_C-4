19ed71a28e68e1306b7fc9de0830cfc15912a0df
MinhNH57 <nutmsotn04@gmail.com>
2024-08-20 23:55:32 +0700

    xác thực

--- ASP.NET Core MVC (C#4)/DemoGH_OnTap/obj/Debug/net6.0/DemoGH_OnTap.AssemblyInfo.cs
+++ ASP.NET Core MVC (C#4)/DemoGH_OnTap/obj/Debug/net6.0/DemoGH_OnTap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoGH_OnTap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f51d1d07bd5dcf4dc9ae96cc873504c0560e35ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoGH_OnTap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoGH_OnTap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Idennity/Idennity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Idennity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
